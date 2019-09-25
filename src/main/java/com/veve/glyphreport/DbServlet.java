268396edc954db91c3e79de6cda3dc16a4591f35
oudun <8adagris>
2019-09-25 12:06:20 +0300

    DB connection check

---
+++ src/main/java/com/veve/glyphreport/DbServlet.java
@@ -0,0 +1,26 @@
+package com.veve.glyphreport;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            String dbUrl = System.getenv("JDBC_DATABASE_URL");
+            System.out.println("JDBC_DATABASE_URL " + dbUrl);
+            Connection conn = DriverManager.getConnection(dbUrl);
+            System.out.println(conn.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
