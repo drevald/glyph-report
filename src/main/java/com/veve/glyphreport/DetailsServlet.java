e4a90d2565e2647d9bb19ae2106f14126cb2b5ad
oudun <oudun@mail.ru>
2019-09-29 08:30:32 +0300

    File upload implemented

---
+++ src/main/java/com/veve/glyphreport/DetailsServlet.java
@@ -0,0 +1,38 @@
+package com.veve.glyphreport;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+
+
+public class DetailsServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    private Logger logger = Logger.getLogger(DetailsServlet.class);
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("!!! DETAILS SERVLET - HEADERS:");
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while(headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            logger.debug(headerName + ":" + req.getHeader(headerName));
+        }
+        logger.info("!!! DETAILS SERVLET - BODY:");
+        InputStream is = req.getInputStream();
+        byte[] buffer = new byte[100];
+        while(is.read(buffer) != -1) {
+            logger.debug(new String(buffer));
+        }
+        logger.info("!!! DETAILS SERVLET - END");
+
+    }
+
+}
