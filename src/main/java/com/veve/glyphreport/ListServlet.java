98f0738b72ea6e2a73cc6bb61061366fdace4113
oudun <oudun@mail.ru>
2020-01-25 01:09:46 +0300

    columns sequence fixed

--- src/main/java/com/veve/glyphreport/ListServlet.java
+++ src/main/java/com/veve/glyphreport/ListServlet.java
@@ -16,14 +16,16 @@ public class ListServlet extends DatabaseServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         List<Report> reports = new ArrayList<Report>();
         try {
-            PreparedStatement ps = conn.prepareStatement("select id_col, app_version_col, created_col from reports_tbl");
+            PreparedStatement ps = conn.prepareStatement("select id_col, app_version_col, created_col, device_model, sdk from reports_tbl");
             ResultSet resultSet = ps.executeQuery();
             System.out.println("resultSet.getFetchSize() is " + resultSet.getFetchSize());
             while(resultSet.next()) {
                 reports.add(new Report(
                         resultSet.getInt("id_col"),
                         resultSet.getString("app_version_col"),
-                        resultSet.getTimestamp("created_col")));
+                        resultSet.getTimestamp("created_col"),
+                resultSet.getString("device_moder"),
+                        resultSet.getString("sdk")));
             }
             req.setAttribute("reports", reports);
         } catch (Exception e) {
